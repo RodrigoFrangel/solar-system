288268ef9835ec3ce47e2a41d91eb213a41d2848
Rodrigo Frangel <soumuitorodrigo@gmail.com>
2022-05-13 01:41:28 -0300

    Feito requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.css';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <Header />
+    );
   }
 }
 
