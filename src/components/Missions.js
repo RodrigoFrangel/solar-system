8cf995b1e4e7892b88a0bbcf036935f628b7e74f
Rodrigo Frangel <soumuitorodrigo@gmail.com>
2022-05-19 15:48:13 -0300

    Last commit (this better work now)

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <section className="beautiful-missions">
+          {missions.map((mission) => (
+            <MissionCard
+              key={ mission.name }
+              name={ mission.name }
+              year={ mission.year }
+              country={ mission.country }
+              destination={ mission.destination }
+            />
+          ))}
+        </section>
       </div>
     );
   }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PlanetCard from './PlanetCard';
-import Planets from '../data/planets';
+import planets from '../data/planets';
 import Title from './Title';
 
 class SolarSystem extends React.Component {
@@ -9,7 +9,7 @@ class SolarSystem extends React.Component {
       <div data-testid="solar-system">
         <Title headline="Planetas" />
         <div className="beautiful-planets">
-          {Planets.map((planet) => (
+          {planets.map((planet) => (
             <PlanetCard
               key={ planet.name }
               planetName={ planet.name }
